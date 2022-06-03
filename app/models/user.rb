9dde4cea31793cfaea16cb127aa11c7ac3430a4a
Bruno Silva <=>
2022-06-03 17:21:51 -0300

    feature/user models and validations

---
+++ app/models/user.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  devise :confirmable,
+         :lockable,
+         :timeoutable,
+         :trackable,
+         :registerable,
+         :recoverable,
+         :rememberable,
+         :database_authenticatable, authentication_keys: [:username]
+
+  validates :first_name, :last_name, presence: true, length: { in: (2..128) }
+
+  EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i
+  validates :username, presence: true, format: { with: EMAIL_REGEX }
+  validates :password, presence: true, length: { in: (6..32) }
+end
