455da79e0ad2cb8dde9a690ea74bff67af29987e
Bruno Silva <=>
2022-06-03 17:22:10 -0300

    feature/role model and validations

---
+++ db/migrate/20220603201442_create_roles.rb
@@ -0,0 +1,14 @@
+# frozen_string_literal: true
+
+class CreateRoles < ActiveRecord::Migration[7.0]
+  def change
+    create_table :roles do |t|
+      t.string :name, limit: 128, index: true
+      t.text :description
+      t.boolean :active, default: true
+
+      t.timestamps
+      t.datetime :deleted_at, index: true
+    end
+  end
+end
