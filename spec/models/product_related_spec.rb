c0248ef1c05d4d60de5b283b808e76de54c66df0
Bruno Silva <=>
2022-06-16 11:37:10 -0300

    hotfix/product related table name coverage

--- spec/models/product_related_spec.rb
+++ spec/models/product_related_spec.rb
@@ -3,6 +3,10 @@
 require 'rails_helper'
 
 RSpec.describe ProductRelated, type: :model do
+  context 'methods' do
+    it { expect(described_class.table_name).to eq('product_related') }
+  end
+
   context 'associations' do
     it { should belong_to(:product) }
     it { should belong_to(:record).class_name('Product') }
